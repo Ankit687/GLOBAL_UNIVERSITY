676f497d0f63c5e04f47bdcbcce254cadea29e5e
Ankit Kumar <Ankit687kumar@gmail.com>
2022-11-23 00:20:58 +0530

    first commit

---
+++ src/main/java/com/umbrella/umbrella_univesity/UmbrellaUnivesityApplication.java
@@ -0,0 +1,13 @@
+package com.umbrella.umbrella_univesity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrellaUnivesityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbrellaUnivesityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umbrella/umbrella_univesity/UmbrellaUnivesityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umbrella.umbrella_univesity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrellaUnivesityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
